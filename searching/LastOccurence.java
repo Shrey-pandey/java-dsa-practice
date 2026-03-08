2038bfd8b3b82b71ce687dda9449da8b9b76ff6f
Shrey-pandey <shrey0408pandey@gmail.com>
2026-03-08 14:59:41 +0530

    added the implementation of last occcurence of the entered element in array

---
+++ searching/LastOccurence.java
@@ -0,0 +1,40 @@
+package searching;
+import java.util.Scanner;
+
+public class LastOccurence {
+    static int binarysearch(int arr[],int k,int beg , int end) {
+        if (beg > end) {
+            return -1;
+        }
+        int mid = beg + (end - beg) / 2;
+        if (arr[mid] == k) {
+            if (mid == arr.length-1 || k != arr[mid +1]) {
+                return mid;
+            } else {
+                return binarysearch(arr, k, mid+1,end);
+            }
+        }
+        if(k<arr[mid]){
+            return binarysearch(arr,k,beg,mid-1);
+        }
+        return binarysearch(arr,k,mid+1,end);
+    }
+    public static void main(String args[]) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of array");
+        n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("enter the elements in the array");
+        for (int i = 0; i < n; i++) {
+            System.out.println("enter the element at the index" + i);
+            arr[i] = sc.nextInt();
+        }
+        int beg = 0, end = arr.length - 1;
+        int k;
+        System.out.println("enter the element to be searched");
+        k = sc.nextInt();
+        int ans = binarysearch(arr,k,beg,end);
+        System.out.println("the element entered has last occured at index"+ans);
+    }
+}
